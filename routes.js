82b9929ee9fa590c1593e8972a22422d6cb4295d
MARCELO ROMEU GONCALVES <marcelo.goncalves.indra@petrobras.com.br>
2020-11-04 14:34:47 -0300

    Commit inicial

---
+++ routes.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var router = express.Router();
+
+router.get("/", function (req, res) {
+    res.render("index");
+});
+
+router.get("/about", function (req, res) {
+    res.render("about");
+});
+
+router.get("/login", function (req, res) {
+    res.render("login");
+});
+
+module.exports = router;
